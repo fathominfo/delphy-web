9c3617ce6f5708f4f3414e4405a88275a2915c43
Mark Schifferli <mark@fathom.info>
2025-02-10 09:26:32 -0500

    prevent QC panel from raising errors on restart

--- src/ts/ui/qcpanel.ts
+++ src/ts/ui/qcpanel.ts
@@ -1,10 +1,13 @@
 import { SharedState } from "../sharedstate";
 
-const parentDiv = document.querySelector("#qc") as HTMLDivElement;
-const metadataDiv = parentDiv.querySelector("#qc--metadata") as HTMLDivElement;
-const sequencesDiv = parentDiv.querySelector("#qc--sequences") as HTMLDivElement;
-const otherDiv = parentDiv.querySelector("#qc--other") as HTMLDivElement;
+const qcDiv = document.querySelector("#qc") as HTMLDivElement;
+const metadataDiv = qcDiv.querySelector("#qc--metadata") as HTMLDivElement;
+const sequencesDiv = qcDiv.querySelector("#qc--sequences") as HTMLDivElement;
+const otherDiv = qcDiv.querySelector("#qc--other") as HTMLDivElement;
 
+
+
+const listItemTemplates : {[key: string]: HTMLLIElement} = {};
 
 /*
 executed once upon completing the load of the data
@@ -41,7 +44,7 @@ export const setQCPanel = (sharedState: SharedState)=>{
 
 const setDateIssues = (noDateSequences:string[])=>{
   /* might need to add logic to hide this if we end up tracking other metadata issues */
-  const ul = parentDiv.querySelector("#qc--dates ul") as HTMLUListElement;
+  const ul = qcDiv.querySelector("#qc--dates ul") as HTMLUListElement;
   noDateSequences.forEach(seq=>{
     const li = document.createElement("li") as HTMLLIElement;
     li.textContent = seq;
@@ -52,18 +55,22 @@ const setDateIssues = (noDateSequences:string[])=>{
 
 
 /*
-@param formatter: takes input and returns a string. At this point in time, the inputs are of type
-InvalidGapWarning, InvalidMutationWarning, InvalidStateWarning, SiteAmbiguity.
-These all hold information about various QC errors, and the formatter function transforms these
-into legible strings for display.
-
+@param formatter: takes input and returns a string. At this point in time, the
+inputs are of type InvalidGapWarning, InvalidMutationWarning, InvalidStateWarning,
+SiteAmbiguity. These all hold information about various QC errors, and the
+formatter function transforms these into legible strings for display.
 */
-const setQCDiv = (cssSelector:string, seqs: {[seqId: string]: any[] }, formatter:(data:any)=>string)=>{ // eslint-disable-line  @typescript-eslint/no-explicit-any
-  const div = parentDiv.querySelector(cssSelector) as HTMLDivElement;
+const setQCDiv = (cssSelector:string, seqs: {[seqId: string]: any[] }, // eslint-disable-line  @typescript-eslint/no-explicit-any
+  formatter:(data:any)=>string)=>{ // eslint-disable-line  @typescript-eslint/no-explicit-any
+  const div = qcDiv.querySelector(cssSelector) as HTMLDivElement;
   let anyEntries = false;
   const ul = div.querySelector("ul") as HTMLUListElement;
-  const template = ul.querySelector("li") as HTMLLIElement;
-  template.remove();
+  let template = listItemTemplates[cssSelector];
+  if (template === undefined) {
+    template = ul.querySelector("li") as HTMLLIElement;
+    template.remove();
+    listItemTemplates[cssSelector] = template;
+  }
   Object.entries(seqs).forEach(([seq, data])=>{
     const li = template.cloneNode(true) as HTMLLIElement;
     const seqSpan = li.querySelector(".seq") as HTMLSpanElement;
